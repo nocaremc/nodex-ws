6cb3406e28aea27640cac391f9e92d0da7d77ae7
Joshua Rodarte <ptc4free@gmail.com>
2018-01-25 22:02:48 -0800

    distinctive error output

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 'use strict'
 const WebSocket = require('ws')
-const ws = new WebSocket('wss://la.dexnode.net/ws', {perMessageDeflate: false})
+const ws = new WebSocket('wss://la.ddexnode.net/ws', {perMessageDeflate: false})
 
+// Based on this bit of simple https://stackoverflow.com/a/41407246
+// LATER: create or use a logging library with colors
+const colors = {
+    reset: "\x1b[0m",
+    fgRed: "\x1b[31m",
+    fgGreen: "\x1b[32m",
+    fgYellow: "\x1b[33m",
+    fgWhite: "\x1b[37m"
+}
+
+// Print to console log using a color. Revert color after.
+function log(error, color)
+{
+    console.log(color, error, colors.reset)
+}
+
+// Print given error to console in red
+function logError(error)
+{
+    log(error, colors.fgRed)
+}
+
+// This promise fires sometime after/during the websocket being established
 ws.on('open', () => {})
+
+// Shit broke
+ws.on('error', logError)
+
