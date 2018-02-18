5749e4eac5fef28debbf1d1933707bdf10bba676
Joshua Rodarte <ptc4free@gmail.com>
2018-02-17 23:20:36 -0800

    Renamed id to more clear request_id. Created function that will build + send a request.

--- src/Connection.js
+++ src/Connection.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws')
 
 class Connection extends WebSocket {
+    
     /**
      * Converts a request object to JSON and sends it
      * @param {object} request 
@@ -9,9 +10,16 @@ class Connection extends WebSocket {
         super.send(JSON.stringify(request))
     }
 
-    buildRequest(api_id, id, method, params) {
+    /**
+     * Return a request prepared to be sent to active rpc node
+     * @param {string} api_id Graphene API ID
+     * @param {string} request_id 
+     * @param {string} method Graphene method name
+     * @param {array} params Graphene method parameters
+     */
+    buildRequest(api_id, request_id, method, params) {
         return {
-            id: id,
+            id: request_id,
             method: "call",
             params: [
                 api_id,
@@ -19,6 +27,18 @@ class Connection extends WebSocket {
                 params
             ]
         }
+    }
+
+    /**
+     * Send a request prepared to be sent to active rpc node
+     * @param {string} api_id Graphene API ID
+     * @param {string} request_id 
+     * @param {string} method Graphene method name
+     * @param {array} params Graphene method parameters
+     */
+    request(api_id, request_id, method, params) {
+        let request = this.buildRequest(api_id, request_id, method, params)
+        this.send(request)
     }
 }
 
